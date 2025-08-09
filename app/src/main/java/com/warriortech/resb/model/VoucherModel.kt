aeca70e7594e0eece7c8c7eff684153c69a3ca9f
KingTecSolutions <kingtecsolution@gmail.com>
2025-08-09 19:35:57 +0530

    Update VoucherModel.kt

--- app/src/main/java/com/warriortech/resb/model/VoucherModel.kt
+++ app/src/main/java/com/warriortech/resb/model/VoucherModel.kt
@@ -13,13 +13,18 @@ data class TblVoucherRequest(
 data class TblVoucherResponse(
     var voucher_id: Long,
     var counter:TblCounter,
+    var voucherType:TblVoucherType,
     var voucher_name:String,
     var voucher_prefix:String,
     var voucher_suffix:String,
     var starting_no:String,
-    var is_active: Boolean
+    var is_active:Boolean
 )
-
+data class TblVoucherType(
+    var voucher_Type_id: Long,
+    var voucher_type_name:String,
+    var is_active:Boolean,
+)
 data class TblVoucher(
     var voucher_id: Long,
     var counter:TblCounter,
